d66e5aa82003425198c2d8b314d0334d5474d441
Edwin Torres <edwin_torres@icloud.com>
2021-01-27 07:09:19 -0500

    Implemented basic button functions.
    When pressing a number, it is displayed in the output.
    
    When pressing the decimal, it only allows 1 decimal to be displayed in the output. It also can not be the last entry which has been limited to 9.
    
    The Clear button removes everything from the output display and displays a "0".

--- CalculatorFirefly/ViewController.swift
+++ CalculatorFirefly/ViewController.swift
@@ -7,14 +7,66 @@
 
 import UIKit
 
+enum Operation: String {
+    case Add = "+"
+    case Subtract = "-"
+    case Divide = "/"
+    case Multiply = "*"
+    case NULL = "Null"
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var outputLabel: UILabel!
+    
+    var runningNum = ""
+    var leftValue = ""
+    var rightValue = ""
+    var result = ""
+    var currentOperation: Operation = .NULL
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        outputLabel.text = "0"
         
     }
-
-
+    
+    @IBAction func numPressed(_ sender: CircleButton) {
+        if runningNum.count <= 8 {
+            runningNum += "\(sender.tag)"
+            outputLabel.text = runningNum
+        }
+    }
+    
+    @IBAction func clearPressed(_ sender: Any) {
+        runningNum = ""
+        leftValue = ""
+        rightValue = ""
+        result = ""
+        currentOperation = .NULL
+        outputLabel.text = "0"
+    }
+    @IBAction func decimalPressed(_ sender: Any) {
+        if runningNum.count <= 7 && !runningNum.contains(".") {
+            runningNum += "."
+            outputLabel.text = runningNum
+        }
+    }
+    @IBAction func equalPressed(_ sender: Any) {
+        
+    }
+    @IBAction func plusPressed(_ sender: Any) {
+        
+    }
+    @IBAction func minusPressed(_ sender: Any) {
+        
+    }
+    @IBAction func multiplyPressed(_ sender: Any) {
+        
+    }
+    @IBAction func dividePressed(_ sender: Any) {
+        
+    }
 }
 
